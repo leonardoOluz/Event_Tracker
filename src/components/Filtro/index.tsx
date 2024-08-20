4247630e8fd67a3b8d452de6c4bd77be3832b7bc
Leonardo Luz <leonardoluz10@hotmail.com>
2024-08-20 18:27:19 -0300

    feat: :white_check_mark: Criado filtro por status no state seletores.

--- src/components/Filtro/index.tsx
+++ src/components/Filtro/index.tsx
@@ -4,16 +4,17 @@ import { useSetRecoilState } from 'recoil';
 import { IFiltrosDeEventos } from '../../interfaces/IFiltroDeEventos';
 import { filtrosDeEventos } from '../../state/atom';
 
-const Filtro: React.FC= () => {
-  
+const Filtro: React.FC = () => {
+
   const [data, setData] = useState('')
+  const [status, setStatus] = useState('ambos');
+  const filtro: IFiltrosDeEventos = {};
+
   const setFiltrosDeEventos = useSetRecoilState<IFiltrosDeEventos>(filtrosDeEventos);
-  
+
   const submeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
-    
     evento.preventDefault()
-    const filtro: IFiltrosDeEventos = {};
-    
+    filtro.status = status;
     if (data) {
       filtro.data = new Date(data);
     } else {
@@ -22,16 +23,24 @@ const Filtro: React.FC= () => {
     setFiltrosDeEventos(filtro);
   };
 
+  const handlechangeSelect = (evento: React.ChangeEvent<HTMLSelectElement>) => {
+    setStatus(evento.target.value);
+  }
+
   return (<form className={style.Filtro} onSubmit={submeterForm}>
     <h3 className={style.titulo}>Filtrar por data</h3>
-    <input 
-      type="date" 
+    <input
+      type="date"
       name="data"
       className={style.input}
-      onChange={evento => setData(evento.target.value)} 
+      onChange={evento => setData(evento.target.value)}
       placeholder="Por data"
       value={data} />
-
+    <select value={status} onChange={handlechangeSelect} className={style.input}>
+      <option autoFocus value="ambos">Todos eventos</option>
+      <option value="completos">Eventos completos</option>
+      <option value="incompletos">Eventos incompletos</option>
+    </select>
     <button className={style.botao}>
       Filtrar
     </button>
