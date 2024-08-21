074431aa0fc7ab8827c6aa86a12dcbae70dbe655
Leonardo Luz <leonardoluz10@hotmail.com>
2024-08-20 21:36:28 -0300

    feat: :bookmark: Concluído o curso de Recoil da Alura.

--- src/state/seletores/index.ts
+++ src/state/seletores/index.ts
@@ -1,29 +1,43 @@
 import { selector } from "recoil";
 import { filtrosDeEventos, listaDeEventosState } from "../atom";
+import { IEvento } from "../../interfaces/IEvento";
 
 export const eventosFiltradosState = selector({
-    key: 'eventosFiltradosState',
-    get: ({ get }) => {
-        const filtro = get(filtrosDeEventos)
-        const todosOsEventos = get(listaDeEventosState)
-        let eventos = todosOsEventos.filter(evento => {
-            if (!filtro.data) {
-                return true
-            }
-            const ehOMesmoDia = filtro.data.toISOString().slice(0, 10) === evento.inicio.toISOString().slice(0, 10);
-            return ehOMesmoDia;
-        });
+  key: 'eventosFiltradosState',
+  get: ({ get }) => {
+    const filtro = get(filtrosDeEventos)
+    const todosOsEventos = get(listaDeEventosState)
+    let eventos = todosOsEventos.filter(evento => {
+      if (!filtro.data) {
+        return true
+      }
+      const ehOMesmoDia = filtro.data.toISOString().slice(0, 10) === evento.inicio.toISOString().slice(0, 10);
+      return ehOMesmoDia;
+    });
 
-        eventos = eventos.filter(evento => {
-            if (filtro.status === 'completos') {
-                return evento.completo === true;
-            }
-            if (filtro.status === 'incompletos') {
-                return evento.completo === false;
-            }
-            return evento;
-        });
+    eventos = eventos.filter(evento => {
+      if (filtro.status === 'completos') {
+        return evento.completo === true;
+      }
+      if (filtro.status === 'incompletos') {
+        return evento.completo === false;
+      }
+      return evento;
+    });
 
-        return eventos
-    }
+    return eventos
+  }
 });
+
+export const eventosAsync = selector({
+  key: 'eventosAsync',
+  get: async () => {
+    const responseHttp = await fetch('http://localhost:8080/eventos');
+    const eventos: IEvento[] = await responseHttp.json();
+    return eventos.map(evento => ({
+      ...evento,
+      inicio: new Date(evento.inicio),
+      fim: new Date(evento.fim)
+    }))
+  }
+})
